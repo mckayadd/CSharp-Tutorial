1696f2394fc837c689da07d9d2416d0586479016
mckayadd <mckaya@ceng.metu.edu.tr>
2026-02-10 00:02:12 +0100

    Data types example added.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+
+int customerAge = 25;
+int itemsInCart = 3;
+int employeeId = 12847;
+
+string customerName = "John Doe";
+string emailAddress = "john.doe@email.com";
+
+bool isCustomerActive = true;
+
+decimal productPrice = 30.55m; // monetary value -> m
+double height = 10.5d;
+
+char grade = 'A';
+
+System.Console.WriteLine(customerName.ToUpper());
+
+// var automatically determines the data type based on the value
+var inferredAge = 30;
+var inferredStatus = true;
+
+System.Console.WriteLine($"Price: ${productPrice:F2}");
+
+/// <summary>
+/// increases the number of items in the cart
+/// </summary>
+itemsInCart += +1;
+
