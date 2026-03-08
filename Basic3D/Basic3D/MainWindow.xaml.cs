958b12779125e3b1ae76d71d23238cec4966de5d
mckayadd <mckaya@ceng.metu.edu.tr>
2026-03-08 14:42:41 +0100

    Basic 3D cube in WPF added

---
+++ Basic3D/Basic3D/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace RobotCubeWPF;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
