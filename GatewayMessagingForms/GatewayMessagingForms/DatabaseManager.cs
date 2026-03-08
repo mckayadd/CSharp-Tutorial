d54d604ef86f202e3dd7cdf8216912e7e9f11c5e
mckayadd <mckaya@ceng.metu.edu.tr>
2026-03-08 01:40:18 +0100

    SQLite added

---
+++ GatewayMessagingForms/GatewayMessagingForms/DatabaseManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite; // Yeni kütüphane
+using System.IO;
+
+public class DatabaseManager
+{
+    private string _dbPath = "SystemLogs.db";
+    private string _connectionString => $"Data Source={_dbPath};"; // "Version=3"e gerek yok
+
+    public DatabaseManager()
+    {
+        if (!File.Exists(_dbPath))
+        {
+            // Microsoft.Data.Sqlite dosyayı bağlantı açıldığında otomatik oluşturur
+            using (var conn = new SqliteConnection(_connectionString))
+            {
+                conn.Open();
+                string sql = @"CREATE TABLE Logs (
+                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                Category TEXT,
+                                Message TEXT
+                             )";
+                using (var cmd = new SqliteCommand(sql, conn)) { cmd.ExecuteNonQuery(); }
+            }
+        }
+    }
+
+    public void SaveLog(string message, string category)
+    {
+        using (var conn = new SqliteConnection(_connectionString))
+        {
+            conn.Open();
+            string sql = "INSERT INTO Logs (Category, Message) VALUES (@cat, @msg)";
+            using (var cmd = new SqliteCommand(sql, conn))
+            {
+                cmd.Parameters.AddWithValue("@cat", category);
+                cmd.Parameters.AddWithValue("@msg", message);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- GatewayMessagingForms/GatewayMessagingForms/LoggerService.cs
+++ GatewayMessagingForms/GatewayMessagingForms/LoggerService.cs
@@ -5,13 +5,23 @@ using System.Text;
 namespace GatewayMessagingForms;
 
 public enum LogCategory { Camera, Connector, Robot }
+
 public class LoggerService
 {
     public event Action<string, LogCategory> OnLogRequest;
+    private readonly DatabaseManager _dbManager;
 
+    public LoggerService()
+    {
+        _dbManager = new DatabaseManager();
+    }
     public void Log(string message, LogCategory category)
     {  
+        // Send message to the UI (Event)
         OnLogRequest?.Invoke(message, category);
+
+        // Save message into the DB (saving in the background for improved performance)
+        Task.Run(() => _dbManager.SaveLog(message, category.ToString()));
     }
 
 }
